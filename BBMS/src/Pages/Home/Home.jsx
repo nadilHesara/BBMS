72743c167b3d04662413e3cb32faf56cb121d448
HesandiSM <hesandism@gmail.com>
2025-07-15 23:58:29 +0530

    Add the CompReg page for campaign registrations

--- BBMS/src/Pages/Home/Home.jsx
+++ BBMS/src/Pages/Home/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './Home.css';
 import NaviBar from '../../components/Navibar/NaviBar';
-
+import { Link } from 'react-router-dom';
 
 const Home = ({ theme, setTheme }) => {
   return (
     <div>
       <NaviBar theme={theme} setTheme={setTheme} />
       <h1>Welcome to the Blood Bank Management System</h1>
+      <div>
+        Planning on organizing a blood donation campaign....
+        <br />
+        <Link to='/compReg'>Register</Link>
+
+      </div>
     </div>
   )
 }
