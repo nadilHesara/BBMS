36232bfe03670e7b9bd489a5831775fdfcbe1b58
nadilHesara <nadilhesara@gmail.com>
2025-07-16 01:08:44 +0530

    Donor Reg Completed and styles added

--- BBMS/src/Pages/DonorReg/DonorReg.jsx
+++ BBMS/src/Pages/DonorReg/DonorReg.jsx
@@ -1,67 +1,84 @@
 import NaviBar from "../../components/Navibar/NaviBar";
+import './DonorReg.css'
 
 function DonorReg({ theme, setTheme }) {
+
+  const districts = [
+    "Ampara", "Anuradhapura", "Badulla", "Batticaloa", "Colombo", "Galle", "Gampaha",
+    "Hambantota", "Jaffna", "Kalutara", "Kandy", "Kegalle", "Kilinochchi", "Kurunegala",
+    "Mannar", "Matale", "Matara", "Monaragala", "Mullaitivu", "Nuwara Eliya", "Polonnaruwa",
+    "Puttalam", "Ratnapura", "Trincomalee", "Vavuniya"
+  ];
+
+
   return (
     <div>
       <NaviBar theme={theme} setTheme={setTheme} />
-    <form>
-      
-      <h1>Donor Registration</h1>
-      <label for="Dname"> Donor Name: </label>
-      <input type="text" id="Dname" name="Dname"></input>
-      <br />
+      <div className={theme == 'light' ? "donor-reg" : "donor-reg dark"}>
+        <form>
 
-      <label for="Gender"> Gender: </label>
-      <input type="radio" id="Male" name="Gender" value="" />
-      <label for="Male">Male</label>
-      <input type="radio" id="Female" name="Gender" value="" />
-      <label htmlFor="Female">Female</label>
-      <br />
+          <h1>Donor Registration</h1>
+          <label for="Dname"> Donor Name: </label>
+          <input type="text" id="Dname" name="Dname"></input>
+          <br />
 
-      <label for="BGroup">Blood Group: </label>
-      <select name="BloodGrp" id="BloodGrp">
-        <option value="-"> </option>
-        <option value="A+">A+</option>
-        <option value="B+">B+</option>
-        <option value="O+">O+</option>
-        <option value="AB+">AB+</option>
-        <option value="A-">A-</option>
-        <option value="B-">B-</option>
-        <option value="O-">O-</option>
-        <option value="AB-">AB-</option>
-      </select>
-      <br />
+          <label for="Gender"> Gender: </label>
+          <input type="radio" id="Male" name="Gender" value="" />
+          <label for="Male">Male</label>
+          <input type="radio" id="Female" name="Gender" value="" />
+          <label htmlFor="Female">Female</label>
+          <br />
 
-      <label for="NIC"> NIC Number: </label>
-      <input type="text" id="NIC" name="NIC"></input>
-      <br />
+          <label for="BGroup">Blood Group: </label>
+          <select name="BloodGrp" id="BloodGrp">
+            <option value="-">--Select Blood Group--</option>
+            <option value="A+">A+</option>
+            <option value="B+">B+</option>
+            <option value="O+">O+</option>
+            <option value="AB+">AB+</option>
+            <option value="A-">A-</option>
+            <option value="B-">B-</option>
+            <option value="O-">O-</option>
+            <option value="AB-">AB-</option>
+          </select>
+          <br />
 
-      <label for="DOB"> Date of Birth: </label>
-      <input type="date" id="DOB" name="DOB"></input>
-      <br />
+          <label for="NIC"> NIC Number: </label>
+          <input type="text" id="NIC" name="NIC"></input>
+          <br />
 
-      <label for="Telephone"> Telephone: </label>
-      <input type="tel" id="Telephone" name="Telephone"></input>
-      <br />
+          <label for="DOB"> Date of Birth: </label>
+          <input type="date" id="DOB" name="DOB"></input>
+          <br />
 
-      <label for="Address1"> Address Line 1: </label>
-      <input type="text" id="Address1" name="Address1"></input>
-      <br />
+          <label for="Telephone"> Telephone: </label>
+          <input type="tel" id="Telephone" name="Telephone"></input>
+          <br />
+          <div style={{ display: "flex", alignItems: "flex-start" }}>
+            <label htmlFor="Address" style={{ width: "130px", paddingTop: "5px" }}>
+              Address:
+            </label>
 
-      <label for="Address2"> Address Line 2: </label>
-      <input type="text" id="Address2" name="Address2"></input>
-      <br />
+            <div style={{ display: "flex", flexDirection: "column", gap: "10px", flex: 1 }}>
+              <input type="text" id="Address1" name="Address1" placeholder="Address Line 1" />
+              <input type="text" id="Address2" name="Address2" placeholder="Address Line 2" />
+              <input type="text" id="Address3" name="Address3" placeholder="Address Line 3" />
+            </div>
+          </div>
 
-      <label for="Address3"> Address Line 3: </label>
-      <input type="text" id="Address3" name="Address3"></input>
-      <br />
+          <label for="District"> District: </label>
+          <select id="District" name="District">
+            <option value="">-- Select District --</option>
+            {districts.map((district, index) => (
+              <option key={index} value={district}>{district}</option>
+            ))}
+          </select>
+          <br />
 
-      <label for="District"> District: </label>
-      <input type="text" id="District" name="District"></input>
-      <br />
 
-      <input type="submit" value="Save"></input>
-    </form>
+          <input type="submit" value="Register"></input>
+        </form>
+      </div>
     </div>
   );
 }
--- BBMS/src/Pages/Login/Login.jsx
+++ BBMS/src/Pages/Login/Login.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css';
 import NaviBar from '../../components/Navibar/NaviBar';
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai"; 
+
 
 const Login = ({ theme, setTheme }) => {
+
+  const [show, setShow] = useState(false)
+
+  function toggleShow(){
+    setShow(!show)
+  }
+
   return (
     <div>
       <NaviBar theme={theme} setTheme={setTheme} />
-      <h1>Login</h1>
+      <form>
+        <h1>Donor Login</h1>
+        <label for="Username"> Username: </label>
+        <input type="text" id="Username" name="Username"></input>
+        <br />
+
+        <label for="pwd">Password:</label>
+        <input type={show ? "text":"password"} id="pwd" name="pwd"></input>
+        {show?<AiFillEyeInvisible onClick={() => toggleShow()}/> : <AiFillEye  onClick={() => toggleShow()}/>}
+        <br/>
+
+      </form>
     </div>
   )
 }
