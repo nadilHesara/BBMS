9cb86d4f7e80c1c2f8b3ffff2eca15084c2b0060
nadilHesara <nadilhesara@gmail.com>
2025-07-29 15:04:29 +0530

    set home to previous version

--- BBMS/src/Pages/Home/Home.jsx
+++ BBMS/src/Pages/Home/Home.jsx
@@ -1,66 +1,35 @@
-import React, { useState, useEffect } from "react";
-import "./Home.css";
-import NaviBar from "../../components/Navibar/NaviBar";
-import { Link } from "react-router-dom";
-import LeftSlideBar from "../../components/LeftSlideBar/LeftSlideBar";
-import { useNavigate } from "react-router-dom";
-import Loader from "../../components/Loader/Loader";
+import React from 'react'
+import './Home.css';
+import NaviBar from '../../components/Navibar/NaviBar';
+import { Link } from 'react-router-dom';
+import LeftSlideBar from '../../components/LeftSlideBar/LeftSlideBar';
+import { useNavigate } from 'react-router-dom';
+import Loader from '../../components/Loader/Loader';
 
 const Home = ({ theme, setTheme }) => {
   const navigate = useNavigate();
-
-  const [userType, setUserType] = useState("");
-  const [username, setUsername] = useState("");
-
-  useEffect(() => {
-    const storedUserType = localStorage.getItem("userType");
-    const storedUsername = localStorage.getItem("username");
-    console.log("userType:", storedUserType); //set during log in
-    if (storedUserType === "Doner" || storedUserType === "Hospital") {
-      setUserType(storedUserType);
-      setUsername(storedUsername || "User");
-    }
-  }, []);
-
   const handleClick = () => {
-    navigate("/123");
-  };
+    navigate('/123')
+  }
   return (
-    <div className="app-container">
+    <div className='app-container'>
       <NaviBar theme={theme} setTheme={setTheme} />
 
-      <div className="main-layout">
-        {(userType === "Doner" || userType === "Hospital") && (
-          <>
-            {console.log("Rendering LeftSlideBar for", userType)}
-            <LeftSlideBar
-              theme={theme}
-              userType={userType}
-              username={username}
-            />
-          </>
-        )}
-
-        <div className="content-area">
+      <div className='main-layout'>
+        <div className='content-area'>
           <h1>Welcome to the Blood Bank Management System</h1>
           <div>
             Planning on organizing a blood donation campaign....
-            <button
-              onClick={handleClick}
-              className="btn btn-primary"
-              style={{ marginLeft: "50px" }}
-            >
-              Register
-            </button>
-            <br />
-            <br />
+            <button onClick={handleClick} className='btn btn-primary' style={{ marginLeft: "50px" }}>Register</button>
+            <br /><br />
             <p>Be someone's hope. Donate blood</p>
           </div>
-          <Loader />
+          <Loader/>
         </div>
+
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
