a096115419d0cb18315c50668ce0440563fce90b
Thilokya Angeesa <thilokyaangeesa@gmail.com>
2025-08-14 15:29:14 +0530

    Add changePassword Function and Campaign history service functions

--- BBMS/src/App.jsx
+++ BBMS/src/App.jsx
@@ -11,6 +11,7 @@ import AvailableBloodStocks from "./Pages/AvailableBloodStocks/AvailableBloodSto
 import PageNotFound from "./Pages/PageNotFound/PageNotFound";
 import ForgotPassword from "./Pages/ForgotPassword/ForgotPassword";
 import DonationHistory from "./Pages/DonationHistory/DonationHistory";
+import CampaignHistory from "./Pages/CampaignHistory/CampaignHistory";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -55,7 +56,7 @@ function App() {
               <Route path="hospitalReg" element={<HospitalReg theme={theme} setTheme={setTheme} />} />
               <Route path="donation-history" element={<DonationHistory theme={theme} setTheme={setTheme} />} />
               <Route path="donates" element={<Donates theme={theme} setTheme={setTheme} />} />
-
+              <Route path="CampaignHistory" element={<CampaignHistory theme={theme} setTheme={setTheme} />} />
 
             <Route path="DonationForm" element={<DonationFormLayout />}>
               <Route index element={<EligibilityCheck />} />
--- BBMS/src/Pages/CampaignHistory/CampaignHistory.jsx
+++ BBMS/src/Pages/CampaignHistory/CampaignHistory.jsx
@@ -1,3 +1,5 @@
+
+
 export default function CampaignHistory() {
   return (
     <div>
